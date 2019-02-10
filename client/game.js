88adf4cafeee6ec1b71d8e85199a35a0eb12a56b
SirFurness <eric.1334@icloud.com>
2019-02-09 23:35:06 -0800

    Add unit selection
    Drag selection now selects units and clicking on units selects them.

--- client/game.js
+++ client/game.js
@@ -15,6 +15,8 @@ class MenuScene extends Phaser.Scene {
   }
 
   createDragSelection() {
+    this.highlightedObjects = [];
+
     const graphics = this.add.graphics();
     const thickness = 2;
     const color = 0x007ae5;
@@ -24,12 +26,64 @@ class MenuScene extends Phaser.Scene {
       if (pointer.isDown) {
         graphics.clear();
         graphics.lineStyle(thickness, color, alpha);
+
         graphics.strokeRect(pointer.downX, pointer.downY,
           pointer.x - pointer.downX, pointer.y - pointer.downY);
+
+        this.unhighlightObjects();
+
+        const rectangle = this.createRectangleFromPointer(pointer);
+        this.highlightObjects(rectangle);
       }
     });
 
-    this.input.on('pointerup', () => graphics.clear());
+    this.input.on('pointerdown', (pointer) => {
+      this.unhighlightObjects();
+      this.highlightObjects(new Phaser.Geom.Rectangle(pointer.x, pointer.y, 0, 0));
+    });
+
+    this.input.on('pointerup', () => {
+      graphics.clear();
+    });
+  }
+
+  createRectangleFromPointer(pointer) {
+    let width = pointer.x - pointer.downX;
+    let height = pointer.y - pointer.downY;
+    let { downX, downY } = pointer;
+
+    if (width < 0) {
+      downX += width;
+      width = Math.abs(width);
+    }
+    if (height < 0) {
+      downY += height;
+      height = Math.abs(height);
+    }
+
+    return new Phaser.Geom.Rectangle(downX, downY, width, height);
+  }
+
+  unhighlightObjects() {
+    this.highlightedObjects.forEach(object => object.clearTint());
+  }
+
+  highlightObjects(rectangle) {
+    const tintColor = 0x63a4dd;
+
+    this.children.getChildren().forEach((object) => {
+      try {
+        if ((!Phaser.Geom.Rectangle.Intersection(object.getBounds(), rectangle).isEmpty())
+          || Phaser.Geom.Rectangle.ContainsPoint(
+            object.getBounds(), new Phaser.Geom.Point(rectangle.x, rectangle.y),
+          )) {
+          this.highlightedObjects.push(object);
+          object.tint = tintColor;
+        }
+      } catch (err) {
+        // GameObject does not have getBounds
+      }
+    });
   }
 
   update() {
